ff2115883230f0aa13034587b6f6430bd69ea18c
Kyu Sung Sohn <rok.ksohn@gmail.com>
2022-11-08 14:26:17 +0900

    22.11.08 T

--- 5. news-viewer/App.js
+++ 5. news-viewer/App.js
@@ -8,13 +8,12 @@ for(let i = 0; i < $root.length; i++) {
     $root[i].appendChild(navDOM);
     $root[i].appendChild(newsListDOM);
     
-    newsListDOM.updateNewsList('all', 1, 5)
+    newsListDOM.updateNewsList(newsListDOM.category, newsListDOM.page, newsListDOM.size)
 
     
     const newsListDOMProxy = new Proxy(newsListDOM, {
         set: (object, property, value) => {
             object[property] = value;
-            console.log(object['page']);
             return true;
         }, 
         
@@ -34,7 +33,29 @@ for(let i = 0; i < $root.length; i++) {
             let $pg = newsListDOMProxy['page'];
             let $sz = newsListDOMProxy['size'];
             newsListDOMProxy.updateNewsList($cateogry, $pg, $sz);
+            morePages();
         }
     });
+
+    function morePages() {
+        const scrollObserver = $root[i].querySelector('.scroll-observer'); 
+        const options = { root: null, rootMargin: '0px', threshold: 0.5 };
+    
+        const io = new IntersectionObserver((entries) => {
+            if (entries[0].intersectionRatio === 1) return;
+            if (entries[0].isIntersecting) {
+                console.log(`It's working once`);
+                newsListDOMProxy.page++;
+                let $cateogry = newsListDOMProxy['category'];
+                let $pg = newsListDOMProxy['page'];
+                let $sz = newsListDOMProxy['size'];
+                newsListDOMProxy.updateNewsList($cateogry, $pg, $sz);
+            }
+        }, options)
+        
+        io.observe(scrollObserver);
+    }
+
+    morePages();
 }
 
